a6cd5f793c683dc7a023fee831a3a18606457a97
r1000ru <mail@r1000.ru>
2016-06-23 14:53:16 +0300

    css, onReceiveError

--- js/app.js
+++ js/app.js
@@ -1,66 +1,98 @@
-var taAdd = function(data) {
-    var ta = document.getElementById('data');
-    var data = ta.innerHTML + '\n' + data + '\n';
-    ta.innerHTML = data;
-    ta.scrollTop = ta.scrollHeight;
+var dataAdd = function(data, type) {
+    var dataDiv = document.getElementById('data');
+    var p = document.createElement('P');
+    p.innerText = data;
+    if (type) {
+        p.classList.add(type);
+    }
+    dataDiv.appendChild(p);
 };
 
 var init = function() {
-    var conButton = document.getElementById('connectionButton');
-    var sendButton = document.getElementById('sendButton');
+    var hostSubmit = document.getElementById('hostSubmit');
     var hostInput = document.getElementById('hostInput');
+    var hostForm = document.getElementById('hostForm');
+
+    var sendForm = document.getElementById('sendForm');
     var sendInput = document.getElementById('sendInput');
+
     var socketId;
 
-    sendButton.onclick = function() {
-        if(!socketId) {
-            return;
+    // Get last host from local storage
+    chrome.storage.local.get('host', function(res){
+        if(res.host) {
+            hostInput.value = res.host;
         }
-        var data = sendInput.value + '\r\n';
-        var buffer = new ArrayBuffer(data.length);
-        var view = new Uint8Array(buffer);
-        for (var c = 0; c < data.length; c++) {
-            view[c] = data.charCodeAt(c) % 256;
-        }
+    });
 
-        chrome.sockets.tcp.send(socketId, buffer, function(res){
-            console.log(res);
-            taAdd(data);
-        });
-    }
-
-    conButton.onclick = function(){
+    // On connect to host submit
+    hostForm.onsubmit = function(){
+        // If socket connected - disconnect
         if (socketId) {
             chrome.sockets.tcp.close(socketId, function() {
-                taAdd('Closed');
-                conButton.value = 'Connect';
-                sendButton.setAttribute('disabled', 'disabled');
+                hostSubmit.value = 'Connect';
                 socketId = undefined;
+                dataAdd('Socket close', 'ready');
             });
-            return;
+            return false;
         }
 
-        var address = hostInput.value;
         chrome.sockets.tcp.create( {}, function(socketInfo){
             socketId = socketInfo.socketId;
-            taAdd('Socket created');
-            chrome.sockets.tcp.connect(socketId, address.split(':')[0], parseInt(address.split(':')[1]) || 80, function(res) {
+            dataAdd('Socket created' ,'ready');
+            chrome.sockets.tcp.connect(socketId, hostInput.value.split(':')[0], parseInt(hostInput.value.split(':')[1]) || 80, function(res) {
                 if (chrome.runtime.lastError) {
-                    taAdd(chrome.runtime.lastError.message);
                     chrome.sockets.tcp.close(socketId, function() {
-                        taAdd('Closed by error');
-                        conButton.value = 'Connect';
-                        sendButton.setAttribute('disabled', 'disabled');
                         socketId = undefined;
+                        hostButton.value = 'Connect';
+                        dataAdd('Socket closed by error: '+ chrome.runtime.lastError.message, 'error');
                     });
                     return;
                 }
-                conButton.value = 'Disconnect';
-                sendButton.removeAttribute('disabled')
-                taAdd('Socket connected');
+                chrome.storage.local.set({host: hostInput.value});
+                hostSubmit.value = 'Disconnect';
+                dataAdd('Socket connected', 'ready');
+
             });
         });
+        return false;
     };
+
+    // On message submit
+    sendForm.onsubmit = function() {
+        if(!socketId) {
+            dataAdd('Socket is disconnected', 'error');
+            return false;
+        }
+        // Check connection
+        chrome.sockets.tcp.getInfo(socketId, function(info){
+            if (!info.connected) {
+                hostSubmit.value = 'Connect';
+                socketId = undefined;
+                dataAdd('Socket closed by server', 'error');
+                return;
+            }
+            var data = sendInput.value + '\r\n';
+            var buffer = new ArrayBuffer(data.length);
+            var view = new Uint8Array(buffer);
+            for (var c = 0; c < data.length; c++) {
+                view[c] = data.charCodeAt(c) % 256;
+            }
+
+            chrome.sockets.tcp.send(socketId, buffer, function(res){
+                chrome.sockets.tcp.getInfo(socketId, function(info){
+                    if (!info.connected) {
+                        hostSubmit.value = 'Connect';
+                        socketId = undefined;
+                        dataAdd('Socket closed by server', 'error');
+                    }
+                });
+                dataAdd(data, 'outcoming');
+            });
+        });
+        
+        return false;
+    }
 
     chrome.sockets.tcp.onReceive.addListener(function(info){
         if(info.socketId !== socketId) {
@@ -71,7 +103,18 @@ var init = function() {
         view.forEach(function(ch){
             text += String.fromCharCode(ch);
         });
-        taAdd(text);
+        dataAdd(text, 'incoming');
+    });
+    
+    chrome.sockets.tcp.onReceiveError.addListener(function(info){
+        if(info.socketId !== socketId) {
+            return;
+        }
+        if(info.resultCode === -100) {
+            dataAdd('Server reset connection', 'error');
+        } else {
+            dataAdd('Error: ' + nfo.resultCode);
+        }
     });
 };
 document.addEventListener("DOMContentLoaded", init);
